fe8c13358f0e9f093ef3c8dd37db82d81d58208b
AhmedRazaSiddiqui <Ahmedsiddiqui551@gmail.com>
2023-01-04 02:06:17 +0500

    set database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,33 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      migrationsRun: true,
+      autoLoadEntities: true,
+      host: 'localhost',
+      port: 8009,
+      username: 'postgres',
+      password: 'password',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('auth');
+  }
+}
